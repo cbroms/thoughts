429c894dbb8a00ceda9d7945418addf93b1782b6
Christian Broms <cb@christianbroms.com>
2021-08-24 23:02:36 -0700

    cli tool to build and upload to s3 working

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Thoughts
 
 A system for organizing thoughts.
+
+## Setup
+
+You'll need the AWS CLI installed.
+
+Create a new IAM user profile on your account, generating an access key ID and secret access key. Create a new profile with those credentials on your machine:
+
+```
+aws configure --profile thoughts
+```
---
+++ thoughts.sh
@@ -0,0 +1,39 @@
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
+red="\e[0;91m"
+blue="\e[0;94m"
+green="\e[0;92m"
+reset="\e[0m"
+
+thoughts="${green}@@@ thoughts: "
+
+export AWS_PROFILE=thoughts
+
+printf "${thoughts}${blue}Starting wm-api...${reset}"
+
+cd wm-api && npm run start &
+apiprocess=$!
+
+sleep 3 
+
+printf "${thoughts}${blue}Building static site with ltm-generator...${reset}"
+
+cd ltm-generator && npm run build &
+generatorprocess=$!
+
+wait $generatorprocess
+
+kill $apiprocess
+
+printf "${thoughts}${blue}Loading config file...${reset}\n"
+
+. thoughts.config
+
+printf "${thoughts}${blue}Syncing build to object storage...${reset}\n"
+
+cd ltm-generator/build/ && aws s3 sync . $S3_BUCKET --acl public-read --exclude "page-preview/*" --include "page-preview/dist/*"
+
+printf "${thoughts}${blue}Export complete, exiting...${reset}\n"
+
+exit 
