db897c195a6ce058cce9587cb77feb98f5daff44
Christian Broms <cb@christianbroms.com>
2021-10-07 00:29:30 -0700

    fix links in static version

--- ltm-generator/src/routes/thought/[id].json.js
+++ ltm-generator/src/routes/thought/[id].json.js
@@ -15,7 +15,16 @@ export async function get({ params }) {
 			backlinks.push({ ...res, link });
 		}
 
-		thought.content = thought.content.replaceAll('.md', '');
+		thought.content = thought.content.replaceAll(
+			/<\s*a href="(.*?)"[^>]*>(.*?)<\s*\/\s*a>/g,
+			(match, href, content) => {
+				if (href.indexOf('http') !== -1) {
+					return `<a href="${href}">${content} <span class='link-arrow'>&neArr;</span></a>`;
+				} else {
+					return `<a href="/thought/${href.replace('.md', '')}">${content}</a>`;
+				}
+			}
+		);
 
 		return {
 			body: {
