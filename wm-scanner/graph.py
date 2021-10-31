7926fe9cea841fe6ed4c3f37c923c386ed42418c
Christian Broms <cb@christianbroms.com>
2021-10-30 23:56:45 -0700

    add community detection to grapher

--- wm-scanner/graph.py
+++ wm-scanner/graph.py
@@ -3,6 +3,7 @@ import frontmatter
 
 import matplotlib.pyplot as plt
 import networkx as nx
+from networkx.algorithms.community import greedy_modularity_communities
 
 G = nx.Graph()
 
@@ -16,6 +17,11 @@ for name in glob.glob('../wm/*.md'):
 
     for node_dest in links:
         G.add_edge(node, node_dest)
+
+c = list(greedy_modularity_communities(G))
+
+for comm in c:
+    print(list(comm))
 
 nx.draw_kamada_kawai(G, with_labels=True,)
 plt.show()
