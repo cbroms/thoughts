7b635d977ec9391e9acb54ab4b7d0fb162254b59
Christian Broms <cb@christianbroms.com>
2021-12-03 21:03:46 -0800

    add support for building gmi pages and upload to gemini server

--- ltm-generator-gemini/convert.py
+++ ltm-generator-gemini/convert.py
@@ -1,4 +1,5 @@
 import glob
+import re
 import frontmatter
 
 from md2gemini import md2gemini
@@ -11,13 +12,17 @@ for name in glob.glob('../wm/*.md'):
     updated = post.metadata['updated']
     place = post.metadata['place']
 
-    backlinks = "\n".join(list(map(lambda x: "=> {}.gmi".format(x), post.metadata['backlinks'])))
+    backlinks = "\n".join(
+        list(map(lambda x: "=> /thought/{}.gmi".format(x), post.metadata['backlinks'])))
 
-    # todo: add ## Links only if there are forwardlinks 
-    content = "# {}\n\n{}\n Last updated {} in {}\n## Links".format(title, post, updated, place)
+    # todo: add ## Links only if there are forwardlinks
+    content = "# {}\n\n{}\n\nLast updated {} in {}\n## Links".format(
+        title, post, updated, place)
 
-    gem = md2gemini(content, links="at-end", md_links=True)
-    gem += "\n## Backlinks\n\n{}".format(backlinks)
-    print(gem)
+    gem = md2gemini(content, links="at-end", md_links=True,
+                    link_func=lambda l: "/thought/{}".format(l) if not re.search("https?://", l) else l)
+    gem += "\n\n## Backlinks\n\n{}".format(backlinks)
 
-    
+    with open("../ltm-gemini/thought/{}.gmi".format(filename), 'w+') as f:
+        f.write(gem)
+        f.close()
--- thoughts.sh
+++ thoughts.sh
@@ -14,33 +14,44 @@ trap cleanup EXIT
 
 # build the output and sync with object storage
 build() {
-    export AWS_PROFILE=thoughts
+    # export AWS_PROFILE=thoughts
 
-    printf "${thoughts}${blue}Starting wm-api...${reset}"
-    cd wm-api 
-    npm run start &
-    apiprocess=$!
-    cd ..
-    sleep 2
+    # printf "${thoughts}${blue}Starting wm-api...${reset}"
+    # cd wm-api 
+    # npm run start &
+    # apiprocess=$!
+    # cd ..
+    # sleep 2
 
-    printf "${thoughts}${blue}Copying images to static directory...${reset}\n"
-    mkdir -p ltm-generator/static/images/
-    rsync -rlpgoD --checksum --ignore-existing -v wm/images/ ltm-generator/static/images/
+    # printf "${thoughts}${blue}Copying images to static directory...${reset}\n"
+    # mkdir -p ltm-generator/static/images/
+    # rsync -rlpgoD --checksum --ignore-existing -v wm/images/ ltm-generator/static/images/
 
-    printf "${thoughts}${blue}Building static site with ltm-generator...${reset}\n"
-    cd ltm-generator && npm run build &
-    generatorprocess=$!
-    wait $generatorprocess
+    # printf "${thoughts}${blue}Building static site with ltm-generator...${reset}\n"
+    # cd ltm-generator && npm run build &
+    # generatorprocess=$!
+    # wait $generatorprocess
 
-    printf "${thoughts}${blue}Copying build to ltm...${reset}\n"
-    mkdir -p ltm/
-    rsync -rlpgoD --checksum -v --exclude '*.js' --exclude '*.json' ltm-generator/build/ ltm/
+    # printf "${thoughts}${blue}Copying build to ltm...${reset}\n"
+    # mkdir -p ltm/
+    # rsync -rlpgoD --checksum -v --exclude '*.js' --exclude '*.json' ltm-generator/build/ ltm/
 
     printf "${thoughts}${blue}Loading config file...${reset}\n"
     . thoughts.config 
 
-    printf "${thoughts}${blue}Syncing build to object storage...${reset}\n"
-    cd ltm/ && aws s3 sync . $S3_BUCKET --acl public-read --exclude ".DS_Store"
+    # printf "${thoughts}${blue}Syncing build to object storage...${reset}\n"
+    # cd ltm/ && aws s3 sync . $S3_BUCKET --acl public-read --exclude ".DS_Store" && cd ..
+
+    printf "${thoughts}${blue}Building gemini site with ltm-generator-gemini...${reset}\n"
+    mkdir -p ltm-gemini && cd ltm-gemini && mkdir -p thought && cd ..
+    cd ltm-generator-gemini
+    if [ ! -d env ]; then python3 -m venv env && pip install -r requirements.txt; fi
+    source env/bin/activate
+    python convert.py
+    cd ..
+
+    printf "${thoughts}${blue}Syncing gemini build to gemini server...${reset}\n"
+    rsync -rlpgoD --checksum -v ltm-gemini/ $GEMINI_SERVER:$GEMINI_SERVE_DIR
 
     printf "${thoughts}${blue}Export complete, exiting...${reset}\n"
     exit 
