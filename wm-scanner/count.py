1383b6d282020cdadd8db30d1c512dcbb77a5c9d
Christian Broms <cb@christianbroms.com>
2022-01-23 17:24:39 -0800

    count exclaimation marks

--- wm-scanner/count.py
+++ wm-scanner/count.py
@@ -1,10 +1,13 @@
 import glob
 import json
 import frontmatter
+import re
 
 total_links = 0
 total_words = 0
 total_thoughts = 0
+
+total_exclaim = 0
 
 for name in glob.glob('../wm/*.md'):
 
@@ -14,11 +17,16 @@ for name in glob.glob('../wm/*.md'):
     links = post.metadata['forwardlinks']
     total_links += len(links)
     total_words += len(post.content.split(' '))
+    total_exclaim += len(re.findall("!", post.content))
 
 
 print("{} thoughts".format(total_thoughts))
 print("{} links".format(total_links))
 print("{} words".format(total_words))
+
+print("{} exclaimations, {} per thought".format(
+    total_exclaim, total_exclaim / total_thoughts))
+
 
 data = {'thoughts': total_thoughts, 'links': total_links, 'words': total_words}
 
