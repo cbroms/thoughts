6e6ae1538dfbc1e642d9f631fab05e4cfb7ad2c3
Christian Broms <cb@christianbroms.com>
2022-07-28 20:50:24 -0700

    fix broken gemini generator

--- ltm-generator-gemini/convert.py
+++ ltm-generator-gemini/convert.py
@@ -9,25 +9,26 @@ import dateutil.parser
 Generate an index gmi file that contains a banner ascii art image
 and the indexed links from wm/indexed/indexed.json
 """
-with open('../wm/indexed/indexed.json') as f:
+with open("../wm/indexed/indexed.json") as f:
     index = json.load(f)
 
-with open('../wm/counts/current.json') as f:
+with open("../wm/counts/current.json") as f:
     counts = json.load(f)
 
-with open('banner.txt') as f:
+with open("banner.txt") as f:
     banner = f.readlines()
 
-index_links = "\n".join(
-    list(map(lambda x: "=> /thought/{}.gmi".format(x), index)))
+index_links = "\n".join(list(map(lambda x: "=> /thought/{}.gmi".format(x), index)))
 
 formatted_counts = "{} thoughts, {} links, and {} words.".format(
-    counts['thoughts'], counts['links'], counts['words'])
+    counts["thoughts"], counts["links"], counts["words"]
+)
 
 index_content = "```\n{}\n```\n\n{}\n\n{}".format(
-    ''.join(banner), index_links, formatted_counts)
+    "".join(banner), index_links, formatted_counts
+)
 
-with open("../ltm-gemini/index.gmi", 'w+') as f:
+with open("../ltm-gemini/index.gmi", "w+") as f:
     f.write(index_content)
     f.close()
 
@@ -35,34 +36,48 @@ with open("../ltm-gemini/index.gmi", 'w+') as f:
 """
 Generate .gmi files for every thought in wm/
 """
-for name in glob.glob('../wm/*.md'):
-    filename = name.split('/')[2].replace('.md', '')
+for name in glob.glob("../wm/*.md"):
+    filename = name.split("/")[2].replace(".md", "")
 
     post = frontmatter.load(name)
-    title = post.metadata['node']
-    updated = post.metadata['updated']
-    place = post.metadata['place']
+    title = post.metadata["node"]
+    updates = post.metadata["updates"]
+    places = post.metadata["places"]
 
     backlinks = "\n".join(
-        list(map(lambda x: "=> /thought/{}.gmi".format(x), post.metadata['backlinks'])))
+        list(map(lambda x: "=> /thought/{}.gmi".format(x), post.metadata["backlinks"]))
+    )
 
-    if updated:
-        parsed = dateutil.parser.isoparse(updated)
+    if updates != None and updates[-1] != None:
+        parsed = dateutil.parser.isoparse(updates[-1])
         timestamp = parsed.strftime("%a %b %d %Y")
     else:
-        timestamp = ''
+        timestamp = ""
+
+    if places != None:
+        place = places[-1]
+    else:
+        place = ""
+
     # todo: add ## Links only if there are forwardlinks
     content = "# {}\n\n{}\n\nLast updated {} in {}\n## Links".format(
-        title, post, timestamp, place)
+        title, post, timestamp, place
+    )
 
     content = content.replace(".webp", ".jpg")
 
     # strange quirk, it seems md2gemini is first replacing .md with .gmi, then passing them through the link func
     # I probably would have written it the other way around, or ignored md_links when link_func is specified
-    gem = md2gemini(content, links="at-end", md_links=True, indent="   ", plain=True,
-                    link_func=lambda l: "/thought/{}".format(l) if re.search(".gmi", l) else l)
+    gem = md2gemini(
+        content,
+        links="at-end",
+        md_links=True,
+        indent="   ",
+        plain=True,
+        link_func=lambda l: "/thought/{}".format(l) if re.search(".gmi", l) else l,
+    )
     gem += "\n\n## Backlinks\n\n{}\n\n".format(backlinks)
 
-    with open("../ltm-gemini/thought/{}.gmi".format(filename), 'w+') as f:
+    with open("../ltm-gemini/thought/{}.gmi".format(filename), "w+") as f:
         f.write(gem)
         f.close()
